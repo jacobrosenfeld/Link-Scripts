dba26437c8d527c1bdb52492f9f6541f48d2cefd
Jacob Rosenfeld <me@jacobrosenfeld.me>
2025-08-21 12:18:13 -0400

    add ProtectedLayout component for structured layout with footer

---
+++ bulk-links-vercel/components/ProtectedLayout.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { Footer } from './Footer'
+
+interface ProtectedLayoutProps {
+  children: ReactNode
+}
+
+export function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return (
+    <div className="min-h-screen flex flex-col bg-[color:var(--bg)]">
+      <main className="flex-1 container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
